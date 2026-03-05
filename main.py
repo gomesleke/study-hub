aa33df240dee8939859e605102a00b4aaf85d092
gomesleke <voyagerleke@gmail.com>
2026-03-05 18:14:48 -0300

    modificação main.py + estudos de branch git

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
-from core.timer import main
+import os
+from core.timer import main_timer
+
+
+def clear():
+    if os.name == "nt":  # Windows
+        os.system("cls")
+    else:  # Linux / macOS
+        os.system("clear")
+
+    
+def selection():
+    print("Selecione:\n")
+    print("1-Começar Estudos")
+    print("2-Relatório")
+    print("3-Histórico")
+    print("4-Metas")
+    print("5-Configurações")
+    print("6-Sair")
+
+
+def menu():
+
+    clear()
+
+    print("============")
+    print("\nSTUDY TRACKER (CLI)\n")
+    print("============\n")
+
+    selection()
+
+
+def main():
+    menu()
+    
 
 
 
